14c4228342212b5e91920467e1aae6e90360e5b0
raphael <raphael@develodesign.co.uk>
2020-05-23 01:35:25 +0100

    Next.js 9 project with TypeScript Build straightforward API with Next.js API routes  API request handlers, and how to set up dynamic API routes and endpoints

---
+++ pages/api/user.ts
@@ -0,0 +1,11 @@
+import { NextApiHandler} from "next";
+import data from "../../lib/data.json"
+
+const user: NextApiHandler = ((req, res) => {
+
+    res.status(200).json({
+       data
+    });
+});
+
+export default user;
